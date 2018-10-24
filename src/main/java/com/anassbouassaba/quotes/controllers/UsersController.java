58a4dcf3329b5ca6849c261f79cbedd7d7dc2781
Anass Bouassaba <bouassaba.anass@gmail.com>
2018-10-24 05:36:13 +0200

    Added missing annotation

--- src/main/java/com/anassbouassaba/quotes/controllers/UsersController.java
+++ src/main/java/com/anassbouassaba/quotes/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.anassbouassaba.quotes.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class UsersController {
   @Autowired
   private UserRepository userRepository;
   
+  @PostMapping
   public UserDto create(@RequestBody CreateUserDto body) {
     User user = new User();
     user.setFullName(body.getFullName());
